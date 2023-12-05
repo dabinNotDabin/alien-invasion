f049baf976c07455189d346644a459943c8812b5
Dabin Lamming <dabin.lamming@outlook.com>
2023-12-04 21:03:24 -0700

    Make game run fullscreen

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -13,9 +13,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien fkin Invasion")
 
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         self.clock = pygame.time.Clock()
 
